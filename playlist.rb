f1e756bd4ace24206b1611534ff51091e2c58d83
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-29 11:31:33 +0000

    Add PlayList methods

--- playlist.rb
+++ playlist.rb
@@ -1,5 +1,18 @@
 # store the songs in a list
 #add a song to the list
 #remove the last song from the list
+
+require 'song.rb'
 class PlayList
+  def initialize()
+    @playlist = Array.new
+  end
+  
+  def addSong(song)
+    @playlist << song
+  end
+  
+  def removeLast
+    @playlist.pop
+  end
 end
